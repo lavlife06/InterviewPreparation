ddd0d3abdc9eb283d51a836caae5e7db862a8172
lavlife06 <lav7990192369@gmail.com>
2021-07-29 14:41:05 +0530

    add apply_bind_call and debouncing concept

---
+++ apply_bind_call/app.js
@@ -0,0 +1,65 @@
+// 1)                                                                                      FUNCTION BORROWING
+// const person1 = {
+//     firstname: "lav",
+//     lastname: "senghani",
+//     printName: function () {
+//         console.log(this.firstname + " " + this.lastname);
+//     },
+// };
+
+// const person2 = {
+//     firstname: "karan",
+//     lastname: "mandal",
+// };
+
+// person1.printName.call(person2);
+
+/////                                                                                       BETTER WAY
+
+const person1 = {
+    firstname: "lav",
+    lastname: "senghani",
+};
+
+const person2 = {
+    firstname: "karan",
+    lastname: "mandal",
+};
+const printName = function (hometown, state) {
+    console.log(
+        this.firstname + " " + this.lastname + " from " + `${hometown},` + state
+    );
+};
+
+printName.call(person2, "surat", "jahangirpura");
+
+printName.apply(person2, ["surat", "jahangirpura"]);
+
+const printname = printName.bind(person2, "surat", "jahangirpura");
+printname();
+
+//  /// /// // / / /                                                                    NOTES
+
+// 1) The call() method calls a function with a given this value, and arguments provided as an array
+// 2)  apply is similar to call but just passing argument is different
+//  3) bind is similar to call but it is never invoked on its own, we have the power toi use it anywhere
+
+//                                                                                           CHAINING OF CONSTRUCTOR
+// Function.prototype.construct = function (aArgs) {
+//     let oNew = Object.create(this.prototype);
+//     this.apply(oNew, aArgs);
+//     return oNew;
+// };
+
+// function MyConstructor() {
+//     for (let nProp = 0; nProp < arguments.length; nProp++) {
+//         this["property" + nProp] = arguments[nProp];
+//     }
+// }
+
+// let myArray = [4, "Hello world!", false];
+// let myInstance = MyConstructor.construct(myArray);
+
+// console.log(myInstance.property1); // logs 'Hello world!'
+// console.log(myInstance instanceof MyConstructor); // logs 'true'
+// console.log(myInstance.constructor);
