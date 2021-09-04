0ee1111691a4546b9c73018cce369d8a7f3f9151
lavlife06 <lav7990192369@gmail.com>
2021-09-04 21:44:57 +0530

    some changes in functionc/app.js

---
+++ functions/app.js
@@ -0,0 +1,37 @@
+//           //                  //                                                    ///                     ///  Automatically runnig the function
+// (function func(params) {
+//     console.log("'a' function running");
+// })();
+// (function (params) {
+//     console.log("anonymous function running");
+// })();
+
+// console.log(arrowFunc);
+
+// /////////////////////////////////////////////////////////////////////////////////////////////////
+
+// // //  Fucntion Statement or Function Declaration
+//  Note this is hoisted
+function a(params) {
+    console.log(" a called");
+}
+
+//  // // Function Expression
+// Note - not hoisted, because used as a value,error - ReferenceError: Cannot access 'b' before initialization
+const b = function (params) {
+    console.log("b called");
+};
+
+//  // // Named Function Expression
+// Note - not hoisted c and xyz both, because c used as a value and xyz has different scope
+var c = function xyz(params) {
+    console.log(xyz);
+};
+c(); //  =>   ƒ xyz(params) {
+//     console.log(xyz);
+// }
+// xyz() error
+
+//  FUnctioncs arguments and parameteres
+function name1(params1, params2) {}
+name1(5, 2); // Arguments
