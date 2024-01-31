6dddfc2b67c03cb370347c20f60c67d6964633ec
lavlife06 <lav7990192369@gmail.com>
2024-01-31 21:29:01 +0530

    added some stack and queue problems

--- DSA/DSA/StackAndQueue/queue.js
+++ DSA/DSA/StackAndQueue/queue.js
@@ -1,5 +1,5 @@
-const Stack = require("./stack");
 // i.e. -> f(3) - 1 - 2 - 8 - 5(l)-dynamic
+const Stack = require("./stack");
 
 class Node {
   constructor(value) {
@@ -39,6 +39,51 @@ class Queue {
   }
 }
 
+const queue = new Queue();
+
+// Questions
+
+// 1. implement queue using only one stack javascript // Advance level
+// Steps -
+// - pop out each element and then re-add/push the poped element but not the last one
+// - base case if element is the last one then stop recursion and return that element
+class QueueWithStack {
+  constructor() {
+    this.stack = new Stack();
+  }
+
+  // Enqueue operation
+  enqueue(val) {
+    this.stack.push(val);
+  }
+
+  // Dequeue operation
+  dequeue() {
+    // If the stack is empty, the queue is empty
+    if (this.stack.size === 0) {
+      return null;
+    }
+
+    //Base Case
+    if (this.stack.size === 1) {
+      return this.stack.pop();
+    }
+
+    const item = this.stack.pop();
+    // console.log({item, stack: this.stack});
+
+    const result = this.dequeue();
+
+    this.stack.push(item);
+
+    return result;
+  }
+}
+
+// Example usage:
+// const queue = new QueueWithStack();
+
+// 2. implement queue only two stack
 class QueueUsingTwoStacks {
   constructor() {
     this.stack1 = new Stack();
@@ -49,80 +94,37 @@ class QueueUsingTwoStacks {
   }
   dequeue() {
     // if both stacks are empty, return undefined 
-    if (this.stack1.size() === 0 && this.stack2.size () === 0) 
+    if (this.stack1.size === 0 && this.stack2.size === 0) 
     return undefined;
 
     //if stack2 is empty, pop all elements from stack1 to stack? till stack1 is empty
-
-    if (this.stack2.size() === 0) {
-      while (this.stack1.size() !== 0) {
+    //because till now no dequeue operation is done and so we can add all queued val into stack2 
+    //until and unless stack2 size isn't zero just pop from it because in between dequeue their maybe queueing
+    //so you want to remove the present ele from stack2 and then refill it after stack2 size is zero 
+    if (this.stack2.size === 0) {
+      while (this.stack1.size !== 0) {
         this.stack2.push(this.stack1.pop());
       }
     }
     
     // pop and return the element from stack 2 
-    return this.stack2.pop ();
+    return this.stack2.pop();
   }
 }
 
-// Questions
 
-// 1. implement queue using only one stack javascript // Advance level
-class QueueWithStack {
-  constructor() {
-    this.stack = [];
-  }
+// // Example usage:
+// const queue = new QueueUsingTwoStacks();
+// queue.enqueue(1);
+// queue.enqueue(2);
+// queue.enqueue(3);
 
-  // Enqueue operation
-  enqueue(item) {
-    this.stack.push(item);
-  }
+// console.log(queue.dequeue()); // Output: 1
+// console.log(queue.dequeue()); // Output: 2
 
-  // Dequeue operation
-  dequeue() {
-    // If the stack is empty, the queue is empty
-    if (this.stack.length === 0) {
-      return null;
-    }
+// queue.enqueue(4);
+// console.log(queue.dequeue()); // Output: 3
+// console.log(queue.dequeue()); // Output: 4
+// console.log(queue.dequeue()); // Output: null (queue is empty)
 
-    // Pop the top element
-    const item = this.stack.pop();
-
-    // If it was the last element, return it
-    if (this.stack.length === 0) {
-      return item;
-    }
-
-    // Recursively dequeue the rest of the elements
-    const dequeuedItem = this.dequeue();
-
-    // Enqueue the current item after the rest of the elements
-    this.enqueue(item);
-
-    return dequeuedItem;
-  }
-
-  // Helper function to check if the queue is empty
-  isEmpty() {
-    return this.stack.length === 0;
-  }
-
-  // Helper function to get the size of the queue
-  size() {
-    return this.stack.length;
-  }
-}
-
-// Example usage:
-const queue = new QueueWithStack();
-queue.enqueue(1);
-queue.enqueue(2);
-queue.enqueue(3);
-
-console.log(queue.dequeue()); // Output: 1
-console.log(queue.dequeue()); // Output: 2
-
-queue.enqueue(4);
-console.log(queue.dequeue()); // Output: 3
-console.log(queue.dequeue()); // Output: 4
-console.log(queue.dequeue()); // Output: null (queue is empty)
+module.exports = Queue;
--- DSA/DSA/StackAndQueue/stack.js
+++ DSA/DSA/StackAndQueue/stack.js
@@ -1,60 +1,14 @@
+// i.e. -> dynamic-f(3) - 1 - 2 - 8 - 5(l) -- which will be popping out will be first
 // Uses:
 //   Function Invocation
 //   Undo-Redo (Cmd + z)
 //   Routing the history object (Reactjs routing)
+const Queue = require("./queue");
 
-// stack.push(5);
-// stack.push(8);
-// stack.push(2);
-// stack.push(1);
-// stack.push(3);
-// i.e. -> dynamic-f(3) - 1 - 2 - 8 - 5(l)
-
-// class Node {
-//   constructor(value) {
-//     this.value = value;
-//     this.next = null;
-//   }
-// }
-
-// class Stack {
-//   constructor() {
-//     this.first = null;
-//     this.last = null;
-//     this.size = 0;
-//   }
-//   push(val) {
-//     const newNode = new Node(val);
-//     if (!this.first) {
-//       this.first = newNode;
-//       this.last = newNode;
-//     } else {
-//       const temp = this.first;
-//       this.first = newNode;
-//       this.first.next = temp;
-//     }
-//     return ++this.size;
-//   }
-//   pop() {
-//     if (!this.first) return null;
-//     const temp = this.first;
-//     if (this.first === this.last) {
-//       this.last = null;
-//     }
-//     this.first = this.first.next;
-//     this.size--;
-//     return temp.value;
-//   }
-// }
-
-// Questions
-
-// 1. Implement minEle in stack
 class Node {
-  constructor(value, minVal) {
+  constructor(value) {
     this.value = value;
     this.next = null;
-    this.min = minVal;
   }
 }
 
@@ -65,20 +19,15 @@ class Stack {
     this.size = 0;
   }
   push(val) {
-    if(!this.first){
-      const minVal = val;
-      const newNode = new Node(val, minVal);
+    const newNode = new Node(val);
+    if (!this.first) {
       this.first = newNode;
       this.last = newNode;
     } else {
-      const minVal = this.first.min > val ? val : this.first.min;
-      // console.log({minVal, crrFirstMinVal: this.first.min, val});
-      const newNode = new Node(val, minVal);
       const temp = this.first;
       this.first = newNode;
       this.first.next = temp;
     }
-    // console.log({first: this.first, last: this.last});
     return ++this.size;
   }
   pop() {
@@ -88,70 +37,204 @@ class Stack {
       this.last = null;
     }
     this.first = this.first.next;
-    temp.size--;
+    this.size--;
     return temp.value;
-  }
-  minEle() {
-    return this.first?.min ?? 0;
-  }
+  } 
 }
 
-const stack = new Stack();
-stack.push(5); stack.push(8); stack.push(2); stack.push(1); stack.push(3);
+// const stack = new Stack();
+// stack.push(5); stack.push(8); stack.push(2); stack.push(1); stack.push(3);
+// console.log(stack.pop()); console.log(stack.pop()); console.log(stack.pop()); console.log(stack.pop()); console.log(stack.pop()); console.log(stack.pop());
 
-console.log(stack.minEle(), "InitialminVal");
+// Questions
+// 1. Implement minEle in stack
+// class Node {
+//   constructor(value, minVal) {
+//     this.value = value;
+//     this.next = null;
+//     this.min = minVal;
+//   }
+// }
 
-stack.pop(); console.log(stack.minEle(), "minVal");
-stack.pop(); console.log(stack.minEle(), "minVal");
-stack.pop(); console.log(stack.minEle(), "minVal");
-stack.pop(); console.log(stack.minEle(), "minVal");
-stack.pop(); console.log(stack.minEle(), "minVal");
-stack.pop(); console.log(stack.minEle(), "minVal");
+// class Stack {
+//   constructor() {
+//     this.first = null;
+//     this.last = null;
+//     this.size = 0;
+//   }
+//   push(val) {
+//     if(!this.first){
+//       const minVal = val;
+//       const newNode = new Node(val, minVal);
+//       this.first = newNode;
+//       this.last = newNode;
+//     } else {
+//       const minVal = this.first.min > val ? val : this.first.min;
+//       // console.log({minVal, crrFirstMinVal: this.first.min, val});
+//       const newNode = new Node(val, minVal);
+//       const temp = this.first;
+//       this.first = newNode;
+//       this.first.next = temp;
+//     }
+//     // console.log({first: this.first, last: this.last});
+//     return ++this.size;
+//   }
+//   pop() {
+//     if (!this.first) return null;
+//     const temp = this.first;
+//     if (this.first === this.last) {
+//       this.last = null;
+//     }
+//     this.first = this.first.next;
+//     temp.size--;
+//     return temp.value;
+//   }
+//   minEle() {
+//     return this.first?.min ?? 0;
+//   }
+// }
+
+// const stack = new Stack();
+// stack.push(5); stack.push(8); stack.push(2); stack.push(1); stack.push(3);
+
+// console.log(stack.minEle(), "InitialminVal");
+
+// stack.pop(); console.log(stack.minEle(), "minVal");
+// stack.pop(); console.log(stack.minEle(), "minVal");
+// stack.pop(); console.log(stack.minEle(), "minVal");
+// stack.pop(); console.log(stack.minEle(), "minVal");
+// stack.pop(); console.log(stack.minEle(), "minVal");
+// stack.pop(); console.log(stack.minEle(), "minVal");
 
 // 2. How to Sort a Stack using Recursion
-function sortStack(s) {
-  // If the stack is empty or has only one element, return
-  if (s.length <= 1) {
-    return;
+// Step -
+// - designing a recursive algorithm that repeatedly removes elements from the original stack and inserts them back in sorted order
+
+// { stackBB: [ 34 ] }
+// { stackB: [ 3 ], tempStackB: [ 34 ] }
+// { stackA: [ 3, 34 ] }
+// { stackBB: [ 3, 34 ] }
+// { stackB: [ 3, 31 ], tempStackB: [ 34 ] }
+// { stackA: [ 3, 31, 34 ] }
+// { stackBB: [ 3, 31, 34 ] }
+// { stackB: [ 3, 31, 34, 98 ], tempStackB: [] }
+// { stackA: [ 3, 31, 34, 98 ] }
+// { stackBB: [ 3, 31, 34, 98 ] }
+// { stackB: [ 3, 31, 34, 92 ], tempStackB: [ 98 ] }
+// { stackA: [ 3, 31, 34, 92, 98 ] }
+// { stackBB: [ 3, 31, 34, 92, 98 ] }
+// { stackB: [ 3, 23 ], tempStackB: [ 98, 92, 34, 31 ] }
+// { stackA: [ 3, 23, 31, 34, 92, 98 ] }
+
+// const s = [];
+
+// const sortStack = (stack) => {
+//   //Base Case
+//   if (stack.length <= 1) {
+//     return;
+//   }
+
+//   const item = stack.pop();
+
+//   sortStack(stack);
+
+//   const tempStack = [];
+
+//   console.log({stackBB: stack});
+
+//   // Move all elements that are greater than x from the main stack to the tempStack
+//   while (stack.length !== 0 && stack[stack.length - 1] > item) {
+//     tempStack.push(stack.pop());
+//   }
+
+//   stack.push(item);
+
+//   console.log({stackB: stack, tempStackB: tempStack});
+
+//   while (tempStack.length !== 0) {
+//     stack.push(tempStack.pop());
+//   }
+//   console.log({stackA: stack});
+//   return stack;
+// }
+
+// // // Push elements into the stack
+// s.push(34);
+// s.push(3);
+// s.push(31);
+// s.push(98);
+// s.push(92);
+// s.push(23);
+// console.log(sortStack(s));
+ 
+// // Sort the stack
+// sortStack(s);
+
+// 3. Next Greater Value solution
+//Rule - Smaller or equal elements push to top of stack and pop it when found top stack's element < current arr element and keep on poping until the situation conflicts/reverse 
+// const profitsCalculation = (arr) => {
+//   const s = [0]; // store indexes
+//   const profitDays = [];
+
+//   for (let i = 1; i < arr.length; i++) {
+//     const crrArrEleValue = arr[i];
+//     let stacksTopEle = s[s.length - 1]; //index of arr
+//     let stacksTopEleValue = arr[stacksTopEle];
+
+//     while (s.length && stacksTopEleValue < crrArrEleValue) { // this means the current arr element is continuosly greater then the stoack top most low element and so we keep popping and looping till we find greater one
+//       profitDays[stacksTopEle] = i - stacksTopEle;
+//       s.pop();
+
+//       //updating to keep on looping
+//       stacksTopEle = s[s.length - 1]; 
+//       stacksTopEleValue = arr[stacksTopEle];      
+//     }
+
+//     s.push(i); //all the smaller values are poped out and now the current greater one is to be added
+//   } 
+
+//   while (s.length) {
+//     profitDays[s.pop()] = -1;
+//   }
+
+//   return profitDays;
+// }
+
+// console.log(profitsCalculation([13, 7, 6, 12, 10])); //[ -1, 2, 1, -1, -1 ]
+// console.log(profitsCalculation([10, 15, 5, 20, 25, 10, 30])); //[1, 2, 1, 1, 2, 1, -1]
+// console.log(profitsCalculation([10, 9, 6, 5, 4, 3, 20])); //[6, 5, 4, 3, 2, 1, -1]
+
+class StackUsingQueue {
+  constructor() {
+    this.queue = new Queue();
   }
- 
-  // Remove the top element of the stack
-  let x = s.pop();
- 
-  // Sort the remaining elements in the stack using recursion
-  sortStack(s);
- 
-  // Create two auxiliary stacks
-  let tempStack = [];
- 
-  // Move all elements that are greater than x from the main stack to the tempStack
-  while (s.length !== 0 && s[s.length - 1] > x) {
-    tempStack.push(s.pop());
+
+  push(val) {
+    this.queue.enqueue(val);
   }
- 
-  // Push x back into the main stack
-  s.push(x);
- 
-  // Move all elements from tempStack back to the main stack
-  while (tempStack.length !== 0) {
-    s.push(tempStack.pop());
+
+  pop() {
+    const size = this.queue.size;
+
+    if (size === 0) {
+      return null;
+    }
+
+    // Move all elements except the last one to the back of the queue
+    for (let i = 0; i < size - 1; i++) {
+      const item = this.queue.dequeue();
+      this.queue.enqueue(item);
+    }
+
+    // Pop the last element (the one at the front)
+    return this.queue.dequeue();
   }
 }
- 
-// Create a stack
-let s = [];
- 
-// Push elements into the stack
-s.push(34);
-s.push(3);
-s.push(31);
-s.push(98);
-s.push(92);
-s.push(23);
- 
-// Sort the stack
-sortStack(s);
 
+const stack = new StackUsingQueue();
+stack.push(5); stack.push(8); stack.push(2); stack.push(1); 
+console.log(stack.pop()); 
+stack.push(3);
+console.log(stack.pop()); console.log(stack.pop()); console.log(stack.pop()); console.log(stack.pop()); console.log(stack.pop());
 
-module.export = {Stack}
-
+module.exports = Stack
